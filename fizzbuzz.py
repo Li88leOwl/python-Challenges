42de378f96ab879d38a967ba2c0b5081adaa8564
Jeffery Gerald <83908465+Li88leOwl@users.noreply.github.com>
2024-02-15 06:17:10 +0300

    fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -2,7 +2,11 @@
 #if a number is divisible by 3 print(fizz)
 #if a number is divisible bt 5 print(buzz)
 #if a number is divisble by both 3 & 5 print(fizzbuzz)
-for num in range (0, 10000):
+#fizz buzz challenge 
+choice = int(input("Enter you're number of choice: "))
+def fizzbuzz(choice):
+
+ for num in range (0, choice):
     if num % 3 == 0 and num % 5 == 0:
         print("fizzbuzz")
     elif num % 3 == 0:
@@ -12,3 +16,5 @@ for num in range (0, 10000):
     
     else:
         print(num)
+
+fizzbuzz(choice)
