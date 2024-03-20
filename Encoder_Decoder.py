e23392dd2016ee70f7cd9647207c516c2ee852c6
Jeffery Gerald <83908465+Li88leOwl@users.noreply.github.com>
2024-03-20 11:02:54 +0300

    Create Encoder_Decoder.py
    simply a password encryption/decryption using base 64

---
+++ Encoder_Decoder.py
@@ -0,0 +1,20 @@
+#Password encryption program
+#Encoder
+import base64
+def encrypt_pass(password):
+    encoded_bytes = base64.b64encode(password.encode())
+    print(encoded_bytes)
+
+
+
+user_pass = input("Enter your password: ")
+encrypt_pass(user_pass)
+
+#Decoder
+def decode_pass(password):
+    decode_bytes = base64.b64decode(password)
+    decode_data = decode_bytes.decode()
+    print(f"decode password {decode_data}")
+
+encode_string = input("Enter the base64 string: ")
+decode_pass(encode_string)
